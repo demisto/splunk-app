a865d67f8b10883142339bf62c2d8012be6ecbf7
hirshbergshachar <shachar@demisto.com>
2018-06-04 12:30:30 +0300

    Add-on refactoring

---
+++ add-on/TA-Demisto/bin/demisto_config.py
@@ -0,0 +1,82 @@
+#
+# This code was written by Demisto Inc
+#
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+import re
+import requests
+import splunk.version as ver
+
+version = float(re.search("(\d+.\d+)", ver.__version__).group(1))
+
+try:
+    if version >= 6.4:
+        from splunk.clilib.bundle_paths import make_splunkhome_path
+    else:
+        from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
+except ImportError as e:
+    raise ImportError("Import splunk sub libraries failed\n")
+
+maxbytes = 20000
+
+
+class DemistoConfig(object):
+    def __init__(self, logger):
+        self.logger = logger
+
+    @classmethod
+    def get_logger(cls, logger_name):
+        """
+        This method is used to create a logger object
+
+        :param logger_name:
+        :return: logger object
+        """
+        log_path = make_splunkhome_path(["var", "log", "demisto"])
+        if not (os.path.isdir(log_path)):
+            os.makedirs(log_path)
+
+        handler = RotatingFileHandler(os.path.join(log_path + '/demisto.log'), maxBytes=maxbytes, backupCount=20)
+
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.INFO)
+
+        logger.addHandler(handler)
+        return logger
+
+    def validate_token(self, url, authkey, verify_cert, ssl_cert_loc=None):
+        """
+        This method is used to validate Authorization token. It takes four arguments:
+
+        :param url: Demisto URL, its mandatory parameter.
+        :param authkey: authkey for authentication.
+        :param verify_cert: If SSC is to be used
+        :param ssl_cert_loc: Location of the public key of the SSC
+        :return:
+        """
+        headers = {
+            'Authorization': authkey,
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        if verify_cert and ssl_cert_loc is None:
+            # logger.info("Passing verify = False")
+            # r = requests.get(url = url, verify = False,allow_redirects = True, headers = headers)
+            # todo change the ssl verification mechanism
+            self.logger.info("Using default value for verify = True")
+            r = requests.get(url=url, verify=True, allow_redirects=True, headers=headers)
+        else:
+            self.logger.info("Passing verify=" + str(ssl_cert_loc))
+            r = requests.get(url=url, verify=ssl_cert_loc or True,
+                             allow_redirects=True, headers=headers)
+
+        self.logger.info("Token Validation Status:" + str(r.status_code))
+        if 200 <= r.status_code < 300 and len(r.content) > 0:
+            return True, str(r.status_code)
+
+        return False, str(r.status_code)
--- add-on/TA-Demisto/bin/demisto_setup.py
+++ add-on/TA-Demisto/bin/demisto_setup.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
 #
-# This code is written by Demisto Inc
+# This code was written by Demisto Inc
+#
 
-# import your required python modules
 import json
 import re
 import splunk.admin as admin
 import splunk.rest
 
-from demisto_alert import get_logger
-from demisto_alert import validate_token
+from demisto_config import DemistoConfig
 
 # Logging configuration
 maxbytes = 2000000
 
-logger = get_logger("DEMISTOSETUP")
+DEMISTO_PASSWORD_ENDPOINT = "/servicesNS/nobody/TA-Demisto/admin/passwords?search=TA-Demisto&output_mode=json"
+PORT_REGEX = "^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"
+IP_REGEX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+DOMAIN_REGEX = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+
+logger = DemistoConfig.get_logger("DEMISTOSETUP")
+demisto = DemistoConfig(logger)
 
 
 class ConfigApp(admin.MConfigHandler):
@@ -24,17 +29,14 @@ class ConfigApp(admin.MConfigHandler):
             for arg in ['AUTHKEY', 'DEMISTOURL', 'PORT', 'SSL_CERT_LOC']:
                 self.supportedArgs.addOptArg(arg)
 
-    def getstorage_detail(self):
-        r = splunk.rest.simpleRequest(
-            "/servicesNS/nobody/TA-Demisto/admin/passwords?search=TA-Demisto&output_mode=json",
-            self.getSessionKey(), method = 'GET')
+    def get_app_password(self):
+        r = splunk.rest.simpleRequest(DEMISTO_PASSWORD_ENDPOINT, self.getSessionKey(), method='GET')
         password = ""
 
         if 200 <= int(r[0]["status"]) < 300:
             dict_data = json.loads(r[1])
             if len(dict_data["entry"]) > 0:
                 for ele in dict_data["entry"]:
-
                     if ele["content"]["realm"] == "TA-Demisto":
                         password = ele["content"]["clear_password"]
                         break
@@ -42,28 +44,28 @@ class ConfigApp(admin.MConfigHandler):
         return password
 
     def handleList(self, confInfo):
-        confDict = self.readConf("demistosetup")
+        config_dict = self.readConf("demistosetup")
 
-        for stanza, settings in confDict.items():
+        for stanza, settings in config_dict.items():
             for key, val in settings.items():
                 confInfo[stanza].append(key, val)
 
     '''
     After user clicks Save on setup screen, take updated parameters,
-    normalize them, and save them somewhere
+    normalize them, and save them
     '''
 
     def handleEdit(self, confInfo):
 
-        exceptionRaised = False
-
+        password = ''
 
         if self.callerArgs.data['SSL_CERT_LOC'][0] is None:
             self.callerArgs.data['SSL_CERT_LOC'] = ''
 
         if self.callerArgs.data['PORT'][0] is None:
             self.callerArgs.data['PORT'] = ''
-        elif not re.match("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$", self.callerArgs.data['PORT'][0]):
+
+        elif not re.match(PORT_REGEX, self.callerArgs.data['PORT'][0]):
             logger.exception("Invalid Port Number")
             raise Exception("Invalid Port Number")
 
@@ -74,12 +76,8 @@ class ConfigApp(admin.MConfigHandler):
             logger.info("Auth key found")
             password = self.callerArgs.data['AUTHKEY'][0]
 
-        if not re.match(
-                "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",
-                self.callerArgs.data['DEMISTOURL'][0]) \
-                and not re.match(
-                        "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$",
-                        self.callerArgs.data['DEMISTOURL'][0]):
+        if not re.match(IP_REGEX, self.callerArgs.data['DEMISTOURL'][0]) and not \
+                re.match(DOMAIN_REGEX, self.callerArgs.data['DEMISTOURL'][0]):
             logger.exception("Invalid URL")
             raise Exception("Invalid URL")
 
@@ -91,62 +89,67 @@ class ConfigApp(admin.MConfigHandler):
                 url += ":" + self.callerArgs.data['PORT'][0]
 
             '''
-                Create Test Incident to demisto to verify if the configuration entered are correct
+                Create Test Incident to demisto to verify if the configuration is correct
                 Store configuration only if create incident was successful.
             '''
             url += "/incidenttype"
             if self.callerArgs.data['SSL_CERT_LOC']:
-                valid, status = validate_token(url, password,
-                                       verify_cert = True,
-                                       ssl_cert_loc = self.callerArgs.data['SSL_CERT_LOC'][0])
+                valid, status = demisto.validate_token(url, password,
+                                                       verify_cert=True,
+                                                       ssl_cert_loc=self.callerArgs.data['SSL_CERT_LOC'][0])
             else:
-                valid, status = validate_token(url, password, verify_cert = True)
+                valid, status = demisto.validate_token(url, password, verify_cert=True)
 
             if not valid:
                 logger.info("resp status: " + str(status))
-                postargs = {'severity': 'error', 'name': 'Demisto',
-                            'value': 'Token validation Failed, got status: ' + str(status)
-                            }
+                post_args = {
+                    'severity': 'error',
+                    'name': 'Demisto',
+                    'value': 'Token validation Failed, got status: ' + str(status)
+                }
 
                 splunk.rest.simpleRequest('/services/messages', self.getSessionKey(),
-                                          postargs = postargs)
-                exceptionRaised = True
+                                          postargs=post_args)
 
                 raise Exception('Token validation Failed, got status: ' + str(status))
+
             else:
-                postargs = {'severity': 'info', 'name': 'Demisto',
-                            'value': 'Demisto API key was successfully validated for host ' +self.callerArgs.data['DEMISTOURL'][0]
-                            }
+                post_args = {
+                    'severity': 'info',
+                    'name': 'Demisto',
+                    'value': 'Demisto API key was successfully validated for host ' +
+                             self.callerArgs.data['DEMISTOURL'][0]
+                }
 
                 splunk.rest.simpleRequest('/services/messages', self.getSessionKey(),
-                                          postargs = postargs)
+                                          postargs=post_args)
                 user_name = "demisto"
-                password = self.getstorage_detail()
+                password = self.get_app_password()
                 '''
-                Store password into passwords.conf file. Following are different scenarios
-                1. Enters credentials for first time, use REST call to store it in passwords.conf
-                2. Updates password. Use REST call to update existing password.
-                3. Upadates Username. Delete existing User entry and insert new entry.
+                Store password into passwords.conf file. There are several scenarios:
+                1. Enter credentials for first time, use REST call to store it in passwords.conf
+                2. Update password. Use REST call to update existing password.
+                3. Update Username. Delete existing User entry and insert new entry.
                 '''
                 if password:
-                    postArgs = {
+                    post_args = {
                         "password": self.callerArgs.data['AUTHKEY'][0]
                     }
-                    logger.info("In UPDATE")
+                    logger.info("Updating existing user password")
                     realm = "TA-Demisto:" + user_name + ":"
                     splunk.rest.simpleRequest(
                         "/servicesNS/nobody/" + self.appName + "/admin/passwords/" + realm + "?output_mode=json",
-                        self.getSessionKey(), postargs = postArgs, method = 'POST')
+                        self.getSessionKey(), postargs=post_args, method='POST')
 
                 else:
                     logger.info("Password not found")
-                    postArgs = {
+                    post_args = {
                         "name": user_name,
                         "password": self.callerArgs.data['AUTHKEY'][0],
                         "realm": "TA-Demisto"
                     }
                     splunk.rest.simpleRequest("/servicesNS/nobody/TA-Demisto/admin/passwords/?output_mode=json",
-                                              self.getSessionKey(), postargs = postArgs, method = 'POST')
+                                              self.getSessionKey(), postargs=post_args, method='POST')
 
                 '''
                     Remove AUTHKEY from custom configuration.
@@ -154,17 +157,22 @@ class ConfigApp(admin.MConfigHandler):
                 del self.callerArgs.data['AUTHKEY']
 
                 self.writeConf('demistosetup', 'demistoenv', self.callerArgs.data)
+
         except Exception as e:
-            logger.exception("Exception while createing Test incident, error: " + str(e))
-            
-            postargs = {'severity': 'error', 'name': 'Demisto',
-                        'value': 'Invalid configuration for Demisto, please update configuration for '
-                                 'Splunk-Demisto integration to work, error is: ' + str(e)}
+            logger.exception("Exception while creating Test incident, perhaps something is wrong with your "
+                             "credentials. The error was: " + str(e))
+
+            post_args = {
+                'severity': 'error',
+                'name': 'Demisto',
+                'value': 'Invalid configuration for Demisto, please update configuration for '
+                         'Splunk-Demisto integration to work, error is: ' + str(e)
+            }
             splunk.rest.simpleRequest('/services/messages', self.getSessionKey(),
-                                      postargs = postargs)
+                                      postargs=post_args)
             raise Exception("Invalid Configuration, error: " + str(e))
 
-    def handleReload(self, confInfo = None):
+    def handleReload(self, confInfo=None):
         """
         Handles refresh/reload of the configuration options
         """
