ce7c5dfd88044caf2d8cbe5da3924025d33b8bf0
dtavori <dtavori@paloaltonetworks.com>
2020-08-16 17:29:45 +0300

    build - appinspect - in progress

--- .github/appinspect.py
+++ .github/appinspect.py
@@ -2,8 +2,10 @@ import requests
 import time
 import json
 import argparse
+import os
 
 from requests.auth import HTTPBasicAuth
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class AppInspector:
@@ -22,10 +24,14 @@ class AppInspector:
 
         return response.json().get('data').get('token')
 
-    def http_request(self, method, url, data=None, files=None, raise_errors=True):
-        headers = {'Authorization': f'Bearer {self.token}'}
+    def http_request(self, method, url, headers=None, data=None, raise_errors=True):
+        auth_header = {'Authorization': f'Bearer {self.token}'}
+        if headers:
+            headers.update(auth_header)
+        else:
+            headers = auth_header
 
-        response = requests.request(method, url, headers=headers, data=data, files=files)
+        response = requests.request(method, url, headers=headers, data=data)
 
         if raise_errors:
             response.raise_for_status()
@@ -34,10 +40,16 @@ class AppInspector:
 
     def submit_file(self):
         url = 'https://appinspect.splunk.com/v1/app/validate'
+        file_path = self.spl_path
+        app_name = os.path.basename(file_path)
+        fields = {'app_package': (app_name, open(self.spl_path, 'rb'))}
+        payload = MultipartEncoder(fields=fields)
+        headers = {
+            'Content-Type': payload.content_type,
+            'max-messages': 'all'
+        }
 
-        files = {'app_package': open(self.spl_path, 'rb')}
-
-        response = self.http_request('POST', url, files=files)
+        response = self.http_request('POST', url, headers=headers, data=payload)
 
         self.request_id = response.json().get('request_id')
 
