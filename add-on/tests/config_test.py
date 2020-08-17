45ddb9c92e11f62e9137487cfc2095da0f4d4e73
dtavori <dtavori@paloaltonetworks.com>
2020-08-17 12:11:58 +0300

    build - unittests - in progress

--- add-on/tests/config_test.py
+++ add-on/tests/config_test.py
@@ -1,37 +1,3 @@
-#!/usr/bin/env python3
-# coding=utf-8
-
-#
-# This code was written by Demisto Inc
-#
-
-import sys
-import re
-import splunk.version as ver
-
-# Importing the demisto_config library
-# A.  Import make_splunkhome_path
-# B.  Append library path to sys.path
-# C.  Import DemistoConfig from demisto_config
-
-version = float(re.search(r"(\d+.\d+)", ver.__version__).group(1))
-
-try:
-    if version >= 6.4:
-        from splunk.clilib.bundle_paths import make_splunkhome_path
-    else:
-        from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
-except ImportError:
-    raise ImportError("Import splunk sub libraries failed\n")
-
-sys.path.append(make_splunkhome_path(["etc", "apps", "TA-Demisto", "bin", "lib"]))
-
-try:
-    from demisto_config import DemistoConfig
-except BaseException:
-    sys.exit(3)
-
-
 def test_config():
     assert 1 == 1
 
