734f45a50022a45a4e4dd5c19741b1d8b8482e92
dtavori <dtavori@paloaltonetworks.com>
2020-08-19 21:50:09 +0300

    build - unittests - testing imports

--- add-on/tests/addon_test.py
+++ add-on/tests/addon_test.py
@@ -5,12 +5,12 @@ sys.modules['splunk'] = Mock()
 sys.modules['splunk.rest'] = Mock()
 sys.modules['splunk.clilib'] = Mock()
 # sys.modules['splunk.clilib.bundle_paths.make_splunkhome_path'] = Mock()
-sys.modules['splunk.appserver.mrsparkle.lib.util.make_splunkhome_path'] = Mock()
-# sys.modules['splunk.clilib.cli_common'] = Mock()
+# sys.modules['splunk.appserver.mrsparkle.lib.util.make_splunkhome_path'] = Mock()
+sys.modules['splunk.appserver'] = Mock()
+sys.modules['splunk.admin'] = Mock()
 sys.modules['splunk.version'] = Mock()
+sys.modules['splunk.version.__version__'] = Mock()
 sys.modules['splunk.util'] = Mock()
-# sys.modules['splunk.util.mktimegm'] = Mock()
-# sys.modules['splunk.util.normalizeBoolean'] = Mock()
 
 
 def test_demisto_servers():
