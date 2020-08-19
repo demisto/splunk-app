80c894fa44611366688c69b41173bee6ff2193f1
dtavori <dtavori@paloaltonetworks.com>
2020-08-19 22:05:53 +0300

    build - unittests - testing imports

--- add-on/tests/addon_test.py
+++ add-on/tests/addon_test.py
@@ -1,16 +1,26 @@
 import sys
 from unittest.mock import Mock
 
+
+class MockSplunkVersion:
+    def __init__(self):
+        self.__version__ = '6.5.0'
+
+
+class MockSplunkCliLib:
+    @staticmethod
+    def make_splunkhome_path(lst):
+        return '/'.join(lst)
+
+
 sys.modules['splunk'] = Mock()
 sys.modules['splunk.rest'] = Mock()
 sys.modules['splunk.clilib'] = Mock()
-# sys.modules['splunk.clilib.bundle_paths.make_splunkhome_path'] = Mock()
-# sys.modules['splunk.appserver.mrsparkle.lib.util.make_splunkhome_path'] = Mock()
-sys.modules['splunk.appserver'] = Mock()
+sys.modules['splunk.clilib.bundle_paths'] = Mock(spec=MockSplunkCliLib)
+sys.modules['splunk.appserver.mrsparkle.lib.util'] = Mock(spec=MockSplunkCliLib)
 sys.modules['splunk.admin'] = Mock()
-sys.modules['splunk.version'] = Mock()
-sys.modules['splunk.version.__version__'] = Mock()
 sys.modules['splunk.util'] = Mock()
+sys.modules['splunk.version'] = Mock(spec=MockSplunkVersion)
 
 
 def test_demisto_servers():
