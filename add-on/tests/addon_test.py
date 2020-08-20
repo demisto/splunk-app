fa40eb804845d0d682f5250d07ef21315bbc4dae
dtavori <dtavori@paloaltonetworks.com>
2020-08-20 09:14:29 +0300

    build - unittests - testing imports

--- add-on/tests/addon_test.py
+++ add-on/tests/addon_test.py
@@ -1,10 +1,6 @@
 import sys
-from unittest.mock import Mock
-
-
-class MockSplunkVersion:
-    def __init__(self):
-        self.__version__ = '6.5.0'
+import pytest
+from unittest.mock import Mock, patch, PropertyMock
 
 
 class MockSplunkCliLib:
@@ -16,23 +12,26 @@ class MockSplunkCliLib:
 sys.modules['splunk'] = Mock()
 sys.modules['splunk.rest'] = Mock()
 sys.modules['splunk.clilib'] = Mock()
-sys.modules['splunk.clilib.bundle_paths'] = Mock(spec=MockSplunkCliLib)
-sys.modules['splunk.appserver.mrsparkle.lib.util'] = Mock(spec=MockSplunkCliLib)
+sys.modules['splunk.clilib.bundle_paths'] = Mock(spec_set=MockSplunkCliLib)
+sys.modules['splunk.appserver.mrsparkle.lib.util'] = Mock(spec_set=MockSplunkCliLib)
 sys.modules['splunk.admin'] = Mock()
 sys.modules['splunk.util'] = Mock()
-sys.modules['splunk.version'] = Mock(spec=MockSplunkVersion)
+sys.modules['splunk.version'] = Mock()
 
 
-def test_demisto_servers():
-    from demisto_servers import get_servers_from_response
+def test_demisto_servers(mocker):from demisto_servers import get_servers_from_response
     pass
 
 
 def test_demisto_action():
-    from demisto_send_alert import get_config_from_response
+    with patch('ver') as patched_version:
+        type(patched_version.return_value).__version__ = PropertyMock(return_value="6.5.0")
+        from demisto_send_alert import get_config_from_response
     pass
 
 
 def test_get_validate_ssl_value_from_response():
-    from demisto_setup import get_validate_ssl_value_from_response
+    with patch('ver') as patched_version:
+        type(patched_version.return_value).__version__ = PropertyMock(return_value="6.5.0")
+        from demisto_setup import get_validate_ssl_value_from_response
     pass
